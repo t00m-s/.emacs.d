bc2f5d13f958ac204f56803a477bd9b941a6a265
Tommaso <soncintommaso@gmail.com>
2023-04-13 15:48:19 +0200

    add: dap-mode

---
+++ lisp/init-dap.el
@@ -0,0 +1,8 @@
+;;; init-dap.el --- Dap mode configuration -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+(use-package dap-mode
+  :ensure t)
+
+(provide 'init-dap)
+;;; init-dap.el ends here
